f51ea23871e305b85a7e695201d124502a9388c3
INFO-studio <beta-s@outlook.com>
2025-04-13 14:27:19 +0800

    modify: home tech stack icons

--- src/components/articleComponents/specific/appHome/TechStackIcon.vue
+++ src/components/articleComponents/specific/appHome/TechStackIcon.vue
@@ -12,73 +12,45 @@ const isHover = ref(false)
 </script>
 
 <template>
-  <n-space
+  <n-flex
     :size="0"
-    style="position: relative"
+    justify="center"
+    align="center"
     @mouseenter="isHover = true"
     @mouseleave="isHover = false"
   >
     <n-icon :color="props.color" size="24">
       <component :is="props.icon" />
     </n-icon>
-<!--    <motion.div-->
-<!--      :initial="{-->
-<!--        opacity: 0,-->
-<!--        x: '-100%',-->
-<!--      }"-->
-<!--      :animate="-->
-<!--        isHover-->
-<!--          ? {-->
-<!--              opacity: 1,-->
-<!--              x: 0,-->
-<!--              userSelect: 'text',-->
-<!--            }-->
-<!--          : {-->
-<!--              opacity: 0,-->
-<!--              x: -24,-->
-<!--              userSelect: 'none',-->
-<!--            }-->
-<!--      "-->
-<!--      class="tech-stack-icon-title"-->
-<!--    >-->
-<!--      {{ props.title }}-->
-<!--    </motion.div>-->
-<!--    <motion.div-->
-<!--      :initial="{-->
-<!--        opacity: 0,-->
-<!--        x: -24,-->
-<!--        width: 0,-->
-<!--      }"-->
-<!--      :animate="-->
-<!--        isHover-->
-<!--          ? {-->
-<!--              x: 0,-->
-<!--              width: 'unset',-->
-<!--              marginRight: '0.5rem'-->
-<!--            }-->
-<!--          : {-->
-<!--              x: -24,-->
-<!--              width: 0,-->
-<!--              marginRight: 0-->
-<!--            }-->
-<!--      "-->
-<!--      class="tech-stack-icon-title-background"-->
-<!--    >-->
-<!--      {{ props.title }}-->
-<!--    </motion.div>-->
-  </n-space>
+    <motion.div
+      :initial="{
+        opacity: 0,
+      }"
+      :animate="
+        isHover
+          ? {
+              opacity: 1,
+              y: 24,
+              userSelect: 'text',
+            }
+          : {
+              opacity: 0,
+              y: 0,
+              userSelect: 'none',
+            }
+      "
+      class="tech-stack-icon-title"
+    >
+      {{ props.title }}
+    </motion.div>
+  </n-flex>
 </template>
 
 <style scoped>
 .tech-stack-icon-title {
   position: absolute;
-  margin: 0 0 0 0.5rem;
   white-space: nowrap;
-}
-
-.tech-stack-icon-title-background {
-  position: relative;
-  white-space: nowrap;
-  user-select: none;
+  text-shadow: 0 0 10px;
+  z-index: -1;
 }
 </style>
